e844f85bbb37bf09a5ab3773db64b9c6c6d48de7
Na Rim <skfla07@naver.com>
2023-12-12 21:46:30 +0900

    [feat] query hook 작성

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 'use client';
-
 import { ArrowUpIcon } from '@/public/icons';
 import styled from '@emotion/styled';
+import { useGetShops } from '@/hooks';
 
-const Heading = styled.h1`
-  color: ${({ theme }) => theme.colors.pink_dark};
-`;
+function Home() {
+  const { list } = useGetShops();
 
-export default function Home() {
+  if (!list) return <></>;
   return (
     <main>
       <Heading>홈홈홈홈홈</Heading>
@@ -15,3 +14,9 @@ export default function Home() {
     </main>
   );
 }
+
+export default Home;
+
+const Heading = styled.h1`
+  color: ${({ theme }) => theme.colors.pink_dark};
+`;
