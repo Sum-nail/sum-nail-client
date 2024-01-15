4bf0318d54276b89a5a1508530d094dff8e1a1b9
Na Rim <skfla07@naver.com>
2024-01-15 22:30:38 +0900

    [chore] 임시 토큰 env파일로 이동

--- api/index.tsx
+++ api/index.tsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 export const client = axios.create({
   baseURL: process.env.NEXT_PUBLIC_END,
   headers: {
-    Authorization:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDEsIm5hbWUiOiJoYWhhIiwiaWF0IjoxNjQyMDcwMDI4LCJleHAiOjE2NDQ2NjIwMjgsImlzcyI6IldZQiJ9.F-ExkKt4mAxfQjThsAg-afnmv0HGixblOLLxFZ2U7A',
+    Authorization: process.env.TMP_TOKEN,
   },
 });
